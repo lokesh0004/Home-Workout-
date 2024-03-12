9be1c39cd2ed949381c7210d775199dac65ab0e6
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2024-03-12 17:09:33 +0530

    Converted to React App

---
+++ src/Components/Box.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+   
+       <div class="box"> <a href="./box1.html"> <div class="chest ">box1</div></a>
+         <a href="./box2.html"> 
+        <div class="chest">box1</div>
+         </a>
+        <div class="chest">box1</div>
+        <div class="chest">box1</div>
+        <div class="chest">box1</div>
+        <div class="chest">box1</div>
+        <div class="chest">box1</div>
+        <div class="chest">box1</div>
+        <div class="chest">box1</div>
+    </div>
+  )
+}
+
+export default Box
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div class="navbar">
+        <div class="image"></div>
+        <a class="home">Home</a>
+        <a>Yog asana</a>
+        <a>Diet Plan</a>
+        <a>Info</a>
+        <div class="btn">
+        <button class="loginBtn">Login/Register</button>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar;
